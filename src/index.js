2749dbaf9abeeaf3ae46caf99d37608b0acb7c75
Leonardo A <leo.adriano1994@gmail.com>
2019-07-28 21:52:20 -0300

    Add minimal react and react-dom

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => <div> Works !</div>;
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(<Root />, root);

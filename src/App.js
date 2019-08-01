ca9f0ecfdc521065fc3c8b7112da1b206e84aff2
Leonardo A <leo.adriano1994@gmail.com>
2019-07-31 22:28:26 -0300

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
+import { normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
 const App = () => (
   <BrowserRouter>
+    <GlobalStyle />
     <Router />
   </BrowserRouter>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./build"),
     filename: "[name].[hash].js"
   },
   module: {
